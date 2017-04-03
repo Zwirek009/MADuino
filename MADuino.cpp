21cdfd007e67725b8748a13035eb179a4f4550ef
Zwirek009 <maciek.wiraszka@gmail.com>
2017-04-03 20:52:29 +0300

    initial commit

---
+++ MADuino.cpp
@@ -0,0 +1,10 @@
+/// Project: MADuino
+/// Author: Maciej Wiraszka
+/// Version: v0.01 03.04.17 
+///
+/// MADuino - Arduino library for creating multi-agent systems based on Arduino boards.
+///
+/// File: MADuino.cpp - implementation file for class MADuino that represents a single agent 
+///
+
+#include "MADuino.h"
---
+++ MADuino.h
@@ -0,0 +1,20 @@
+/// Project: MADuino
+/// Author: Maciej Wiraszka
+/// Version: v0.01 03.04.17 
+///
+/// MADuino - Arduino library for creating multi-agent systems based on Arduino boards.
+///
+/// File: MADuino.h - heather file for class MADuino that represents a single agent 
+///
+
+#ifndef MADuino_h
+#define MADuino_h
+
+#include "Arduino.h"
+
+class MADuino
+{
+
+};
+
+#endif
