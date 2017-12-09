5f230f7f7d3f46c25f3c56bb91b8c1117ddf4d00
Zwirek009 <maciek.wiraszka@gmail.com>
2017-12-09 16:39:32 +0200

    #18: add global variables and MADuino setup

--- examples/ABPAlgorithm/ABPAlgorithm.ino
+++ examples/ABPAlgorithm/ABPAlgorithm.ino
@@ -1,9 +1,37 @@
+#include <MADuino.h>
+
+#define ID                      1
+#define NUM_OF_AGENTS           2
+#define NUM_OF_AVAILABLE_COLORS 4
+
+enum color
+{
+    GREEN,
+    RED,
+    YELLOW,
+    BLUE
+};
+
+RF24 radio(9,10);
+RF24Network network(radio);
+
+MADuino agent(&radio, &network, String(ID));
+
+color agentView[NUM_OF_AGENTS];
+color availableColors[NUM_OF_AVAILABLE_COLORS] = {GREEN, RED};
+color currentColor;
+
 void setup() 
 {
+    agent.agentSetup();
 
+    Serial.print("### ABPAlgorithm --> id: ");
+    Serial.print(agent.id);
+    Serial.println(" ###");
+    Serial.println();
 }
 
 void loop() 
 {
-    
+    agent.onLoopStart();
 }
