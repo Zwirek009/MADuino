f4eecaac8a0ded820a48539a67dbe4c9f996143a
Zwirek009 <maciek.wiraszka@gmail.com>
2018-01-17 18:32:22 +0200

    NA: add MsgCounter example program

---
+++ examples/MsgCounter/MsgCounter.ino
@@ -0,0 +1,37 @@
+#include <MADuino.h>
+
+RF24 radio(8,9);
+RF24Network network(radio);
+
+MADuino agent(&radio, &network);
+
+int buttonPin = 2;
+long numOfMsgReceived = 0;
+
+void setup() {
+	agent.agentSetup();
+
+	pinMode(buttonPin, INPUT_PULLUP); // button
+
+	Serial.print("MsgCounter agent started --> id: ");
+	Serial.println(agent.id);
+	Serial.println();
+	Serial.println("Started counting...\n");
+}
+
+void loop() {
+	if ( agent.isMessageReceived() )
+	{
+		numOfMsgReceived++;
+	}
+
+	if ( digitalRead(buttonPin) == LOW )
+	{
+		Serial.print("Total number of msg received:\t");
+		Serial.println(numOfMsgReceived);
+
+		// wait until user unpress the button
+		while (digitalRead(buttonPin) == LOW) {}
+		delay(20);
+	}
+}
